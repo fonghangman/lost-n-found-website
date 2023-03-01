5282fe04407b04cf6ba6280cf19694a72ca658da
Fong Ngai <126634655+fonghangman@users.noreply.github.com>
2023-03-01 04:49:45 +0000

    added a for loop to show items in dictionary. Database not made and is only referencing a dictionary.
    To do:
    Styling
    Database
    Way to update database -- how to insert in the lost and found items so that people can access them
    
    To do after that (polishing)
    make it so that it can insert images and show images if there is any
    add dropdown bar for catagories that update with links/text of items in said catagory that jump/connect to where the item in on the main page

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template, request, flash
 
 app = Flask(__name__)
 
+ITEMS = [
+  {
+    'type' : "1",
+    'id': "1",
+    'location_lastfound': "Sky Pitch",
+    'is_ItemNamed' : True,
+    'item_name': "Black jacket",
+    'nameOnItem':"Fong Hang",
+    'description' : "The name on the jacket is Fong Hang"
+  },
+  {
+    'type' : "2",
+    'id': "1",
+    'location_lastfound': "Sky Pitch",
+    'is_ItemNamed' : False,
+    'item_name': "Blue bottle",
+    'nameOnItem':False,
+    'description' : "The bottle is made of clear plastic"
+  }
+]
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def items_list():
+    return render_template('home.html', 
+                           items=ITEMS)
 print(__name__)
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
